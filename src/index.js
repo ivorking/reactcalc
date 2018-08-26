7c21ac82114f481bac06a2be608c18c865b8bba8
Ivor King <ivorking@gmail.com>
2018-08-26 23:58:41 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Frame from './components/frame';
 import registerServiceWorker from './registerServiceWorker';
-import './styles/main.scss';
+import './styles/main.css';
 import 'jquery';
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
